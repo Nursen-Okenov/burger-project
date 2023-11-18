486ff7787976a3403b28e8130dcaf8a5032b97cb
Nursen-Okenov <nursenokenov57@gmail.com>
2023-11-18 21:27:49 +0600

    write currency code

--- scripts/script.js
+++ scripts/script.js
@@ -40,7 +40,35 @@ document.getElementById("order-action").onclick = function () {
     }
 }
 
+let prices = document.getElementsByClassName("products-item-price");
+document.getElementById("change-currency").onclick = function (e) {
+    let currentCurrency = e.target.innerText;
 
+    let newCurrency = "$";
+    let coefficient = 1;
+
+    if (currentCurrency === "$") {
+        newCurrency = "₽";
+        coefficient = 89.35;
+    } else if (currentCurrency === "₽") {
+        newCurrency = "KGS";
+        coefficient = 89.13;
+    } else if (currentCurrency === "KGS") {
+        newCurrency = "BYN";
+        coefficient = 3;
+    } else if (currentCurrency === 'BYN') {
+        newCurrency = '€';
+        coefficient = 0.9;
+    } else if (currentCurrency === '€') {
+        newCurrency = '¥';
+        coefficient = 6.9;
+    }
+    e.target.innerText = newCurrency;
+
+    for (let i = 0; i < prices.length; i++) {
+        prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
+    }
+}
 
 
 
